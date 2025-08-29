5f8331d369c719fc049d7ac31d99123b4dff60c7
Bharath072005 <bharathmudiraj159@gmail.com>
2025-08-29 20:41:05 +0530

    Create Task4.c

---
+++ Task4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char s1[10];
+    printf("enter the any word \n");
+    scanf("%c",&s1);
+    strrev(s1);
+    printf("reverse:%c",s1);
+}
